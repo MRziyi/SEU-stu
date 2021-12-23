f8bd2506ee08903b628b1ea7a56aef41c12cda20
MRziyi <357408195@qq.com>
2021-12-23 14:35:37 +0800

    21.12.23

--- CODE_Cpp/Cpp_SINGLE/8.b.cpp
+++ CODE_Cpp/Cpp_SINGLE/8.b.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    char a[11]={'\0'};
-    const char *b[12]={"January","Februauy","March","April","May","June","July","August","September","October","November","December"};
+int main()
+{
+    char a[11] = {'\0'};
+    const char *b[12] = {"January", "Februauy", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     gets(a);
-    a[2]='\0';
-    a[5]='\0';
-    char *pm=&a[0], *pd=&a[3],*py=&a[6];
-    printf("%s %s, %s",b[atoi(pm)-1],pd,py);
+    a[2] = '\0';
+    a[5] = '\0';
+    char *pm = &a[0], *pd = &a[3], *py = &a[6];
+    printf("%s %d, %s", b[atoi(pm) - 1], atoi(pd), py);
     return 0;
 }
